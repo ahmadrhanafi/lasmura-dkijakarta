728657a25cac5963ffa0f44e9610fecf0ff2fb06
ahmadrhanafi <ahmadrhanafy87@gmail.com>
2026-03-10 18:45:43 +0700

    try to fix favicon and add new folder on writable

--- app/Views/home/layout/header.php
+++ app/Views/home/layout/header.php
@@ -6,7 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title ?></title>
 
-    <link rel="icon" type="image/svg+xml" href="<?= base_url('assets/favicon/lasmura.jpg') ?>">
+    <link rel="shortcut icon" href="<?= base_url('assets/favicon/lasmura.ico') ?>" type="image/x-icon">
+    <link rel="icon" type="image/x-icon" href="<?= base_url('assets/favicon/lasmura.ico?v=1.1') ?>">
+
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
